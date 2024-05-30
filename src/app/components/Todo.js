41a5d6aacc4a909eab0b5fad273b95a7d8ee7344
Abu Raihan <aburaihancse066@gmail.com>
2024-05-30 12:39:36 +0600

    Edit todo added

--- src/app/components/Todo.js
+++ src/app/components/Todo.js
@@ -2,23 +2,30 @@
 import React, { useState, useEffect } from "react";
 import { MdEditDocument, MdDelete } from "react-icons/md";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
-import { addToDo, removeToDo } from "../redux/features/todos/todoSlice";
+import { addToDo, editToDo, removeToDo } from "../redux/features/todos/todoSlice";
 import toast, { Toaster } from "react-hot-toast";
 
 const Todo = () => {
   const [inputValue, setInputValue] = useState("");
-  const [hydrated, setHydrated] = useState(false); // State to track hydration
+  const [editId, setEditId] = useState(null);
+  const [hydrated, setHydrated] = useState(false); 
   const todoItems = useAppSelector((state) => state.todo.todo);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    setHydrated(true); // Set hydrated to true after component mounts
+    setHydrated(true);
   }, []);
 
   const handleAddTodo = () => {
     if (inputValue.trim()) {
-      dispatch(addToDo(inputValue));
-      toast.success("New item added");
+      if (editId) {
+        dispatch(editToDo({ id: editId, newText: inputValue }));
+        toast.success("Item edited successfully");
+        setEditId(null); // Clear the edit state
+      } else {
+        dispatch(addToDo(inputValue));
+        toast.success("New item added");
+      }
     } else {
       toast.error("Please input something");
     }
@@ -26,12 +33,17 @@ const Todo = () => {
   };
 
   const handleDeleteTodo = (id) => {
-    dispatch(removeToDo(id)); // Ensure you dispatch the action correctly
+    dispatch(removeToDo(id)); 
     toast.success("Item deleted successfully");
   };
 
+  const handleEditTodo = (item) => {
+    setInputValue(item.text);
+    setEditId(item.id);
+  };
+
   if (!hydrated) {
-    return null; // Render nothing until hydration is complete
+    return null; 
   }
 
   return (
@@ -50,17 +62,17 @@ const Todo = () => {
           onClick={handleAddTodo}
           className="bg-orange-200 py-[10px] px-[30px] rounded-lg"
         >
-          Add
+          {editId ? "Edit" : "Add"}
         </button>
       </div>
 
       <div className="my-[50px] mx-[110px]">
         {todoItems &&
-          todoItems.map((item, index) => (
+          todoItems.map((item) => (
             <div key={item.id} className="flex justify-between items-center px-[20px] my-[10px]">
               <h3 className="text-l">{item.text}</h3>
               <div className="flex gap-8">
-                <MdEditDocument className="cursor-pointer text-2xl" />
+                <MdEditDocument onClick={() => handleEditTodo(item)} className="cursor-pointer text-2xl" />
                 <MdDelete onClick={() => handleDeleteTodo(item.id)} className="cursor-pointer text-red-600 text-2xl" />
               </div>
             </div>
