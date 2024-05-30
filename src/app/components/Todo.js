e41ec64505f5e705585a049c5620bdb7d1ee9e33
Abu Raihan <aburaihancse066@gmail.com>
2024-05-30 16:51:26 +0600

    Mejor change added in the UI

--- src/app/components/Todo.js
+++ src/app/components/Todo.js
@@ -2,14 +2,14 @@
 import React, { useState, useEffect } from "react";
 import { MdEditDocument, MdDelete } from "react-icons/md";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
-import { addToDo, editToDo, removeToDo,clearToDo } from "../redux/features/todos/todoSlice";
+import { addToDo, editToDo, removeToDo, clearToDo } from "../redux/features/todos/todoSlice";
 import toast, { Toaster } from "react-hot-toast";
 
 const Todo = () => {
   const [inputTitle, setInputTitle] = useState("");
   const [inputDescription, setInputDescription] = useState("");
   const [editId, setEditId] = useState(null);
-  const [hydrated, setHydrated] = useState(false); 
+  const [hydrated, setHydrated] = useState(false);
   const todoItems = useAppSelector((state) => state.todo.todo);
   const dispatch = useAppDispatch();
 
@@ -17,45 +17,48 @@ const Todo = () => {
     setHydrated(true);
   }, []);
 
-  const handleAddTodo = () => {
-    if (inputTitle.trim()) {
+  const handleAddOrEditTodo = () => {
+    if (inputTitle.trim() && inputDescription.trim()) {
       if (editId) {
-        dispatch(editToDo({ id: editId, newText: inputValue }));
-        toast.success("Item edited successfully");
+        dispatch(editToDo({ id: editId, newTitle: inputTitle, newDescription: inputDescription }));
+        toast.success("Task edited successfully");
         setEditId(null); // Clear the edit state
       } else {
-        dispatch(addToDo(inputTitle, inputDescription));
-        toast.success("New item added");
+        dispatch(addToDo({ inputTitle, inputDescription }));
+        toast.success("New task added");
       }
     } else {
-      toast.error("Please input something");
+      toast.error("Please input both title and description");
     }
     setInputTitle("");
+    setInputDescription("");
   };
 
   const handleDeleteTodo = (id) => {
-    dispatch(removeToDo(id)); 
-    toast.success("Item deleted successfully");
+    dispatch(removeToDo(id));
+    toast.success("Task deleted successfully");
   };
 
   const handleEditTodo = (item) => {
-    setInputValue(item.text);
+    setInputTitle(item.title);
+    setInputDescription(item.description);
     setEditId(item.id);
   };
 
-  const handleClearTodo = ()=>{
-  dispatch( clearToDo());
-  }
+  const handleClearAll = () => {
+    dispatch(clearToDo());
+    toast.success("All items cleared");
+  };
 
   if (!hydrated) {
-    return null; 
+    return null;
   }
 
   return (
     <div className="w-[50%] m-auto p-[10px] my-[20px] bg-gray-100 rounded-xl shadow-xl">
       <Toaster position="top-center" reverseOrder={false} />
       <h1 className="text-center text-2xl font-bold my-[20px]">ToDo List</h1>
-      <div className="flex flex-col justify-center items-center gap-10">
+      <div className="flex flex-col justify-center items-center gap-4">
         <input
           type="text"
           value={inputTitle}
@@ -67,10 +70,10 @@ const Todo = () => {
           value={inputDescription}
           onChange={(e) => setInputDescription(e.target.value)}
           placeholder="Enter task description"
-          className="outline-none border-none bg-white p-[10px] rounded-lg w-[500px]"
+          className="outline-none border-none bg-white p-[10px] rounded-lg w-[500px] h-[100px]"
         />
         <button
-          onClick={handleAddTodo}
+          onClick={handleAddOrEditTodo}
           className="bg-orange-200 py-[10px] px-[30px] rounded-lg"
         >
           {editId ? "Edit Task" : "Add Task"}
@@ -78,23 +81,25 @@ const Todo = () => {
       </div>
 
       <div className="my-[50px] mx-[110px]">
-        {todoItems &&
+        {todoItems.length > 0 ?
           todoItems.map((item) => (
-            <div key={item.id} className="flex flex-col justify-between items-center px-[20px] my-[10px]">
-              <h3 className="text-l">{item.title}</h3>
-              <p className="text-l">{item.description}</p>
+            <div key={item.id} className="flex flex-col justify-between items-center my-[30px] p-[10px] bg-white rounded-xl shadow-xl">
+              <h3 className="text-l font-bold">{item.title}</h3>
+              <p className="text-justify my-[10px]">{item.description}</p>
               <div className="flex gap-8">
                 <MdEditDocument onClick={() => handleEditTodo(item)} className="cursor-pointer text-2xl" />
                 <MdDelete onClick={() => handleDeleteTodo(item.id)} className="cursor-pointer text-red-600 text-2xl" />
               </div>
             </div>
-          ))}
+          )) : <h2 className="text-center p-[20px] text-2xl font-bold">Empty List</h2>}
       </div>
-      <div className="text-center my-[20px]">
-        <button onClick={handleClearTodo} className="bg-red-300 p-[10px] rounded-xl">
-          Clear All Items
-        </button>
-      </div>
+      {todoItems.length > 1 && (
+        <div className="text-center my-[20px]">
+          <button onClick={handleClearAll} className="bg-red-300 p-[10px] rounded-xl">
+            Clear All Items
+          </button>
+        </div>
+      )}
     </div>
   );
 };
